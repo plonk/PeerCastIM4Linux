fd93b5d4d20637fff3e7c3b4072a88c6b0ed9d74
Yoteichi <plonk@piano.email.ne.jp>
2014-09-09 08:51:05 +0900

    changes line ending, deletes trailing whitespace

--- ui/qt4/main.cpp
+++ ui/qt4/main.cpp
@@ -20,7 +20,7 @@
 
 #include <qapplication.h>
 
-#include "peercast.h" 
+#include "peercast.h"
 #include "servmgr.h"
 #include "channel.h"
 #include "version2.h"
@@ -28,7 +28,7 @@
 #ifdef WIN32
 #include "win32/wsys.h"
 #else //WIN32
-#include "unix/usys.h" 
+#include "unix/usys.h"
 #endif //WIN32
 
 #include "main.h"
@@ -62,8 +62,8 @@ public:
 	{
 		return g_iniFilename;
 	}
-	
-	virtual const char * APICALL getPath() 
+
+	virtual const char * APICALL getPath()
 	{
 		static char path[512];
 		QString str = qApp->applicationDirPath();
@@ -71,8 +71,8 @@ public:
 		strcpy(path, str.toLocal8Bit().data());
 		return path;
 	}
-	
-	virtual const char *APICALL getClientTypeOS() 
+
+	virtual const char *APICALL getClientTypeOS()
 	{
 #ifdef WIN32
 		return PCX_OS_WIN32;
@@ -80,11 +80,11 @@ public:
  #ifdef __APPLE__
 		return PCX_OS_MACOSX;
  #else //__APPLE__
- 		return PCX_OS_LINUX;
+        return PCX_OS_LINUX;
  #endif //__APPLE__
 #endif //WIN32
 	}
-	
+
 	virtual void APICALL printLog(LogBuffer::TYPE t, const char *str)
 	{
 		QString temp;
@@ -92,17 +92,17 @@ public:
 		temp += LogBuffer::logTypes[t];
 		temp += "] ";
 		temp += QString::fromUtf8(str);
-		
+
 		printf("%s\n", temp.toLocal8Bit().data());
-		
+
 		g_qLog.push(temp);
 	}
-	
+
 	virtual void APICALL getDirectory()
 	{
 		strcpy(servMgr->modulePath, qApp->applicationDirPath().toLocal8Bit().data());
 	}
-	
+
 	virtual bool clearTemp()
 	{
 		return false;
@@ -111,57 +111,57 @@ public:
 	virtual void APICALL channelUpdate(ChanInfo *info)
 	{
 		QString msg = "";
-		
+
 		if(!info || info->status != ChanInfo::S_PLAY)
 			return;
-		
+
 		if(ServMgr::NT_TRACKINFO & peercastInst->getNotifyMask())
 		{
 			msg += QString::fromUtf8(info->track.artist.data);
-			
+
 			if(!info->track.artist.isSame("") && !info->track.title.isSame(""))
 				msg += " - ";
-			
+
 			msg += QString::fromUtf8(info->track.title.data);
 		}
-			
+
 		if(ServMgr::NT_BROADCASTERS & peercastInst->getNotifyMask())
 		{
 			if(!info->comment.isSame(""))
 			{
 				if(msg != "")
 					msg += "\r\n";
-					
+
 				msg += "\"";
 				msg += QString::fromUtf8(info->comment.data);
 				msg += "\"";
 			}
 		}
-		
+
 		if(msg != "" && b_msg != msg)
 		{
 			tNotifyInfo ninfo;
-			
+
 			b_msg = msg;
-			
+
 			ninfo.type = 0;
 			ninfo.name = QString::fromUtf8(info->name.data);
 			ninfo.msg = msg;
-			
+
 			g_qNotify.push(ninfo);
 		}
 	}
-	
+
 	virtual void APICALL notifyMessage(ServMgr::NOTIFY_TYPE type, const char *msg)
 	{
 		if(ServMgr::NT_PEERCAST & peercastInst->getNotifyMask())
 		{
 			tNotifyInfo ninfo;
-			
+
 			ninfo.type = 0;
 			ninfo.name = "Message from PeerCast";
 			ninfo.msg = QString::fromUtf8(msg);
-			
+
 			g_qNotify.push(ninfo);
 		}
 	}
@@ -170,12 +170,12 @@ public:
 	{
 		g_bChangeSettings = true;
 	}
-	
+
 	QString b_msg;
-	
+
 //	virtual void APICALL channelStart(ChanInfo *info);
 //	virtual void APICALL channelStop(ChanInfo *info);
-//	virtual void APICALL openLogFile();	
+//	virtual void APICALL openLogFile();
 };
 
 #ifdef _APPLE
@@ -185,7 +185,7 @@ OSErr AEHandleRApp(const AppleEvent *event, AppleEvent *reply, long refcon)
 {
 	if(g_mainform)
 		g_mainform->show();
-	
+
     return 0;
 }
 #endif
@@ -207,7 +207,7 @@ int main(int argc, char **argv)
 	str = app.applicationDirPath();
 	str += "/peercast.ini";
 	g_iniFilename.set(str.toLocal8Bit().data());
-	
+
 	for (int i = 1; i < argc; i++)
 	{
 		if(!strcmp(argv[i],"--inifile") || !strcmp(argv[i],"-i"))
@@ -223,48 +223,48 @@ int main(int argc, char **argv)
 			printf("Usage: peercast [options]\n");
 			printf("  -i, --inifile <inifile>  specify ini file\n");
 			printf("  -h, --help               show this help\n");
-			
+
 			return 0;
 		}
 	}
-	
+
 	peercastInst = new MyPeercastInst();
 	peercastApp = new MyPeercastApp();
-	
+
 	peercastInst->init();
-	
+
 	servMgr->getModulePath = false;
-	
+
 	{
 		QMainForm mainform;
-		
+
 		g_mainform = &mainform;
-		
+
 #ifdef _APPLE
 		sRAppHandler = NewAEEventHandlerUPP(AEHandleRApp);
 		AEInstallEventHandler(kCoreEventClass, kAEReopenApplication, sRAppHandler, 0, FALSE);
 #endif
-		
+
 		app.setQuitOnLastWindowClosed(false);
-		
+
 		mainform.show();
 		ret = app.exec();
 
-#ifdef _APPLE		
+#ifdef _APPLE
 		AERemoveEventHandler(kCoreEventClass, kAEReopenApplication, sRAppHandler, FALSE);
 		DisposeAEEventHandlerUPP(sRAppHandler);
 #endif
-		
+
 		g_mainform = NULL;
 	}
-	
+
 	peercastInst->saveSettings();
 	peercastInst->quit();
 
 	sys->sleep(500);
-	
+
 	delete peercastApp;
 	delete peercastInst;
-	
+
 	return ret;
 }

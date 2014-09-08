645ef232bdc803fa513a6908ea3b690e9f9f96b0
Yoteichi <plonk@piano.email.ne.jp>
2014-09-08 22:27:51 +0900

    add documentation for String

--- core/common/sys.h
+++ core/common/sys.h
@@ -36,31 +36,34 @@ class String
 {
 public:
 	enum {
-		MAX_LEN = 256
+		MAX_LEN = 256 //!< 最大バイト数
 	};
 
 	enum TYPE
 	{
-		T_UNKNOWN,
-		T_ASCII,
-		T_HTML,
-		T_ESC,
+		T_UNKNOWN,     //!< 不明。
+		T_ASCII,       //!< ASCII.
+		T_HTML,        //!< HTML文字実体参照を含む。
+		T_ESC,         //!< URL encoded.
 		T_ESCSAFE,
 		T_META,
 		T_METASAFE,
-		T_BASE64,
-		T_UNICODE,
+		T_BASE64,      //!< Base64 encoded.
+		T_UNICODE,     //!< UTF-8.
 		T_UNICODESAFE,
-		T_SJIS, //JP-EX
+        //JP-EX
+		T_SJIS,        //!< Shift-JIS
 	};
 
+    //! デフォルトコンストラクタ。
 	String() {clear();}
+    //! C文字列からのコンストラクタ。
 	String(const char *p, TYPE t=T_ASCII)
 	{
 		set(p,t);
 	}
 
-	// set from straight null terminated string
+	//! Set from straight null terminated string
 	void set(const char *p, TYPE t=T_ASCII)
 	{
 		strncpy(data,p,MAX_LEN-1);
@@ -68,17 +71,17 @@ public:
 		type = t;
 	}
 
-	// set from quoted or unquoted null terminated string
+	//! Set from quoted or unquoted null terminated string
 	void setFromString(const char *str, TYPE t=T_ASCII);
 
-	// set from stopwatch
+	//! Set from stopwatch
 	void setFromStopwatch(unsigned int t);
 
-	// set from time
+	//! Set from time
 	void setFromTime(unsigned int t);
 
 
-	// from single word (end at whitespace)
+	//! Set from single word (end at whitespace)
 	void setFromWord(const char *str)
 	{
 		int i;
@@ -92,7 +95,7 @@ public:
 	}
 
 
-	// set from null terminated string, remove first/last chars
+	//! Set from null terminated string, remove first/last chars
 	void setUnquote(const char *p, TYPE t=T_ASCII)
 	{
 		size_t slen = strlen(p);
@@ -106,38 +109,54 @@ public:
 		type = t;
 	}
 
+    //! 内容を空にする。
 	void clear()
 	{
 		memset(data, 0, MAX_LEN);
 		data[0]=0;
 		type = T_UNKNOWN;
 	}
+    //! URLエスケープする。
 	void ASCII2ESC(const char *,bool);
+    //! HTMLエスケープする。
 	void ASCII2HTML(const char *);
 	void ASCII2META(const char *,bool);
+    //! URLアンエスケープする。
 	void ESC2ASCII(const char *);
 	void HTML2ASCII(const char *);
 	void HTML2UNICODE(const char *);
+    //! Base64デコードする。
 	void BASE642ASCII(const char *);
+    //! 文字コードを自動判別して UTF-8 にする。
 	void UNKNOWN2UNICODE(const char *,bool);
 	void ASCII2SJIS(const char *); //JP-EX
 
 	static	int	base64WordToChars(char *,const char *);
 
+    //! 文字列比較。
 	static bool isSame(const char *s1, const char *s2) {return strcmp(s1,s2)==0;}
 
+    //! 先頭が s に等しい。
 	bool startsWith(const char *s) const {return strncmp(data,s,strlen(s))==0;}
+    //! URLである。
 	bool isValidURL();
+    //! 空である。
 	bool isEmpty() {return data[0]==0;}
+    //! 比較。
 	bool isSame(::String &s) const {return strcmp(data,s.data)==0;}
+    //! 比較。
 	bool isSame(const char *s) const {return strcmp(data,s)==0;}
+    //! 部分一致。大文字小文字の違いは無視される。
 	bool contains(::String &s) {return stristr(data,s.data)!=NULL;}
+    //! 部分一致。大文字小文字の違いは無視される。
 	bool contains(const char *s) {return stristr(data,s)!=NULL;}
+    //! s を自身の最後に追加する。連結すると MAX_LEN - 2 文字を超える場合は何も行わない。
 	void append(const char *s)
 	{
 		if ((strlen(s)+strlen(data) < (MAX_LEN-1)))
 			strcat(data,s);
 	}
+    //! c を自身の最後に追加する。連結すると MAX_LEN - 2 文字を超える場合は何も行わない。
 	void append(char c)
 	{
 		char tmp[2];
@@ -146,6 +165,7 @@ public:
 		append(tmp);
 	}
 
+    //! s を自身の先頭に追加する。連結すると MAX_LEN - 2 文字を超える場合は s にセットされる。
 	void prepend(const char *s)
 	{
 		::String tmp;
@@ -155,18 +175,26 @@ public:
 		*this = tmp;
 	}
 
+    //! 等しい。
 	bool operator == (const char *s) const {return isSame(s);}
+    //! 等しくない。
 	bool operator != (const char *s) const {return !isSame(s);}
 
+    //! const char * にキャストする。
 	operator const char *() const {return data;}
 
+    //! タイプを変更する。
 	void convertTo(TYPE t);
 
+    //! C文字列で得る。
 	char	*cstr() {return data;}
 
+    //! 空白またはタブである。
 	static bool isWhitespace(char c) {return c==' ' || c=='\t';}
 
+    //! タイプ。
 	TYPE	type;
+    //! 内部表現。
 	char	data[MAX_LEN];
 };
 

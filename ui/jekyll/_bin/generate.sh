a125b5137f20664203cf6e46d6981561aaae4a95
Yoteichi <plonk@piano.email.ne.jp>
2014-09-12 21:28:35 +0900

    CSSを作りなおした

--- ui/jekyll/_bin/generate.sh
+++ ui/jekyll/_bin/generate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 for lang in ja en fr de
 do
-    for page in broadcast index relays tree connections logout settings viewlog
+    mkdir -p $lang
+    rm -f $lang/images
+    ln -fs ../_orig/images $lang/images
+    for page in broadcast index relays tree connections logout settings viewlog bcid chaninfo tree
     do
         ln -fs _orig/$page.html $page.$lang.html
     done
